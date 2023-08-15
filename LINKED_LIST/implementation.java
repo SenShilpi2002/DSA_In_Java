0e59b4fbc9a9371c1808e01714a92d022dac70f2
Shilpi <shilpisen072002@gmail.com>
2023-08-15 20:11:33 +0530

    LL

--- LINKED_LIST/implementation.java
+++ LINKED_LIST/implementation.java
@@ -55,6 +55,76 @@ public class implementation {
         return count;
     }
 
+
+
+    //insertAtBeginning->implement a method to insert a node at the start of a linked list
+       void insertAtHead(int data){
+        node temp = new node(data);
+        if(head==null){
+            head = tail =temp;
+        }
+        else{   //non empty
+            temp.next = head;
+            head = temp;
+
+        }
+    }
+
+
+    //insert method->implement a method to insert a node at any given index
+
+    void insertAt(int idx , int data){
+        node newnode = new node(data);
+        node temp = head;
+
+        if(idx==size()){     //this condition willwork only,when we want to add a new node add the end and that last node will become contain the tail value...that time this value will be return;
+            insertAtEnd(data);
+            return;
+        }
+        else{               //if want add a new node in 0 index
+            if(idx==0){
+                insertAtHead(data);
+                return;
+            }
+        }
+        for(int i=1;i<=idx-1;i++){
+            temp=temp.next;
+        }
+        newnode.next = temp.next;
+        temp.next = newnode;
+    } 
+
+
+
+
+
+    //getElementmethod
+    int getAt(int idx){
+        node temp = head;
+        for(int i = 1;i<=idx;i++){
+            temp = temp.next;
+        }
+        return temp.data;
+    }
+
+
+
+
+
+    //deletion method
+    void deleteAt(int idx){
+        node temp = head;
+        if(idx==0){
+            head=head.next;
+            return;
+        }
+        for(int i=1;i<idx-1;i++){
+            temp=temp.next;
+        }
+        temp.next = temp.next.next;
+        tail = temp;
+    }
+
 }
 
 
@@ -62,9 +132,20 @@ public class implementation {
         linkedlist ll = new linkedlist();
 
         ll.insertAtEnd(4); //4
-        ll.insertAtEnd(67); //4 67
+        ll.insertAtEnd(67); //4-> 67
         ll.display();
         System.out.println(ll.size());
-        
+        ll.insertAtEnd(34);  //4->67->34
+        ll.insertAtHead(60);  //4->67->34->60
+        ll.display();
+        ll.insertAt(2, 30); //4->67->30->34->60
+         System.out.println(ll.tail.data);  //here it will return 60
+        ll.insertAt(5,10 );   //4->67->30->34->60->10
+        System.out.println(ll.tail.data); //in this case tail value return the last index value after insert in the end (explain in line 80)
+        ll.insertAt(0,100 );    //line 84
+         System.out.println(ll.getAt(3)); 
+
+         ll.deleteAt(2);
+         ll.display();
     }
 }
