126dc03a1f9514558614084aefca3e89506d8ff4
Shilpi <shilpisen072002@gmail.com>
2023-08-16 23:29:16 +0530

    DLL

---
+++ LINKED_LIST/doublyll.java
@@ -0,0 +1,21 @@
+package LINKED_LIST;
+
+public class doublyll {
+    public static class node{
+        int data;
+        node next;
+        node prev;
+        node(int data){
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+        //4 56 12 7 6
+        node a = new node(4);
+        node b = new node(4);
+        node c = new node(4);
+        node d = new node(4);
+        node e = new node(4);
+    }
+}
