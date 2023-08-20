d62e3aed82cb3ae14065790b02573a60e920a9a5
Shilpi <shilpisen072002@gmail.com>
2023-08-20 19:51:04 +0530

    stack

--- STACK/display.java
+++ STACK/display.java
@@ -36,6 +36,80 @@ public class display {
             st.push(arr[i]);
         }
 
+
+
+        //pushAtBottom
+
+        //normal process
+
+
+        // Stack<Integer> rt = new Stack<>();
+        // int x = 10;
+        // while(st.size()>0){
+        //     rt.push(st.pop());
+        // }
+        // st.push(x);
+        // while(rt.size()>0){
+        //     st.push(rt.pop());
+        // }
+        // System.out.println(st);
+        public static void pushAtBottom(Stack<Integer> st ,int x){
+            if(st.size()==0){
+                st.push(x);
+                return;
+            }
+            int top = st.pop();
+            pushAtBottom(st,x);
+            st.push(top);
+        }
+
+        //display by recursively
+
+        public static void displayRecur(Stack<Integer> st){
+            if(st.size()==0){
+                return;
+            }
+            int top = st.pop();
+            System.out.println(top+" ");
+            displayRecur(st);
+            st.push(top);
+        }
+
+
+        //display reverse by recursively
+
+        /* 
+         * Stack<Integer> rt = new Stack<>();
+         * Stack<Integer> gt = new Stack<>();
+         * while(st.size()>0){
+         * rt.push(st.pop());
+         * }
+         * System.out.println(rt);
+         * while(rt.size()>0){
+         * gt.push(rt.pop());
+         * }
+         * System.out.println(gt);
+         * while(gt.size()>0){
+         * st.push(gt.pop());
+         * }
+         * System.out.println(st);
+         */
+        
+        public static void reverseRecur(Stack<Integer> temp){
+            if(st.size()==1){
+                return;
+            }
+            int top = temp.pop();
+            reverseRecur(temp);
+            pushAtBottom(st,top);
+        }
+
+
+
+
+
+
+
     }
 }
 
