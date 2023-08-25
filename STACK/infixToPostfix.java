cf5faaed7744941d8edf142af96f7704031b0355
Shilpi <shilpisen072002@gmail.com>
2023-08-25 23:13:19 +0530

    prefixandpostfix

---
+++ STACK/infixToPostfix.java
@@ -0,0 +1,7 @@
+package STACK;
+
+public class infixToPostfix {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ STACK/infixToPrefix.java
@@ -0,0 +1,7 @@
+package STACK;
+
+public class infixToPrefix {
+    public static void main(String[] args) {
+        
+    }
+}
