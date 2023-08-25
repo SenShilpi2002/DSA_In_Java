7bff58f04aecf2c57d4640e597a919a33b462a56
Shilpi <shilpisen072002@gmail.com>
2023-08-25 23:11:50 +0530

    infix

---
+++ STACK/infix.java
@@ -0,0 +1,5 @@
+public class infix {
+    public static void main(String[] args) {
+        
+    }
+}
--- shorting/quicksort.java
+++ shorting/quicksort.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class quicksort{
     public static void display(int [] arr){
-        for(int i=0 ; i<arr.length ; i++){
-            System.out.print(arr[i]+" ");
-        }
+        // for(int i=0 ; i<arr.length ; i++){
+        //     System.out.print(arr[i]+" ");
+        // }
 
     }
 
