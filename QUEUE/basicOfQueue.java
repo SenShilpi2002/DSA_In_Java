92b414d25a57ef7b3e35afea3d1df393f04b7122
Shilpi <shilpisen072002@gmail.com>
2023-08-28 11:43:38 +0530

    queue

--- QUEUE/basicOfQueue.java
+++ QUEUE/basicOfQueue.java
@@ -1,7 +1,22 @@
 package QUEUE;
 
+import java.util.LinkedList;
+import java.util.Queue;;
+
 public class basicOfQueue {
     public static void main(String[] args) {
-        
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.add(6);
+        System.out.println(q);
+        q.remove();
+        System.out.println(q);
+        q.poll();
+        System.out.println(q);
+        System.out.println(q.element());  //q.peek()
     }
 }
